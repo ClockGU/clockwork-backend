ffcfd8bc88254d452970116bdde291995a0001d1
Zakriya <83372032+zakriya057@users.noreply.github.com>
2025-04-16 14:49:49 +0200

    Hot fix to update the user roles

--- app/security.py
+++ app/security.py
@@ -21,8 +21,8 @@ with open(PUBLIC_KEY_PATH, "r") as f:
 
 
 class UserRole(Enum):
-    STUDENT = 2
-    SUPERVISOR = 2
+    STUDENT = 0
+    SUPERVISOR = 1
     CLERK = 2
 
 def get_current_supervisor(request: Request):
