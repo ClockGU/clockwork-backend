5b51d89715a44e81eb4f791e3e230affae35e6c1
Zakriya Shakeel <zak57@Zakriyas-MacBook-Air.local>
2025-05-05 19:31:42 +0200

    create an api from which supervisor can send the message

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -5,6 +5,7 @@ from .petition_student import router as petition_student_router
 from .petition_clerk import router as petition_clerk_router
 from .document import router as document_student_router
 from .employee import router as employee_router
+from .email import router as email_router
 
 router = APIRouter()
 
@@ -13,4 +14,4 @@ router.include_router(petition_student_router)
 router.include_router(petition_clerk_router)
 router.include_router(document_student_router)
 router.include_router(employee_router)
-
+router.include_router(email_router)
