4aee5977af8d8a4f4673a9f3f5130da9c2398fb7
Zakriya Shakeel <zak57@Zakriyas-MacBook-Air.local>
2025-10-09 18:44:33 +0200

    1.In student get api also allow petitions with status of clerk revision and awaiting signature to show. 2.Fix the clerk update and revision patch api's

--- api/routers/petition_clerk.py
+++ api/routers/petition_clerk.py
@@ -51,7 +51,7 @@ def update_petition_as_clerk(
     handler: PetitionHandler = Depends(get_petition_handler),
     user=Depends(get_current_clerk)  
 ):
-    updated_petition = handler.update_petition_status_as_clerk(
+    updated_petition = handler.update_petition_as_clerk(
         petition_id=petition_id,
         approved=petition_data.approved
     )
@@ -64,7 +64,7 @@ def request_revision_from_student(
     handler: PetitionHandler = Depends(get_petition_handler),
     user=Depends(get_current_clerk)
 ):
-    updated_petition = handler.update_petition_as_clerk(
+    updated_petition = handler.request_revision_from_student(
         petition_id=petition_id,
         message=revision.message
     )
