9133ebede0d266ef45c05f56ea440c0f2f8f051d
Zakriya Shakeel <zak57@Mac.station>
2025-10-16 21:08:09 +0200

    1. Implement the socket logic 2. Complete the clerk_ervision

--- api/handlers/__init__.py
+++ api/handlers/__init__.py
@@ -2,3 +2,4 @@ from .petition_handler import PetitionHandler
 from .document_handler import StudentDocumentHandler
 from .employee_handler import EmployeeHandler
 from .email_handler import EmailHandler
+from .socker_handler import ConnectionManager
