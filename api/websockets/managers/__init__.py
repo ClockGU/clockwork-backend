c4345697303b8795a69941acbff25c0b7470f805
Christian Grossmüller <chgad.games@gmail.com>
2026-06-15 16:57:34 +0200

    define ConnectionManager Dependecy for FastApi endpoints.

--- api/websockets/managers/__init__.py
+++ api/websockets/managers/__init__.py
@@ -2,3 +2,7 @@ from api.security import auth_clerk_from_token
 from api.websockets.managers.base_manager import WebsocketConnectionManager
 
 ClerkConnectionManager = WebsocketConnectionManager(auth_clerk_from_token)
+
+
+def get_clerk_connection_manager():
+    return ClerkConnectionManager
