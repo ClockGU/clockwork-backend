85f2de322cbc52c23e23573069ab807fb540fa68
Christian Grossmüller <chgad.games@gmail.com>
2026-06-15 16:17:35 +0200

    Implement authentication flow for websocket.

--- api/routers/web_socket.py
+++ api/routers/web_socket.py
@@ -1,10 +1,12 @@
+import asyncio
 from typing import Dict, List
-from fastapi import  (
-    APIRouter, 
+from fastapi import (
+    APIRouter,
     Depends,
-    WebSocket, 
+    WebSocket,
     WebSocketDisconnect
 )
+import logging
 from sqlmodel import Session
 from json import dumps, JSONEncoder
 from uuid import UUID
@@ -15,13 +17,15 @@ from api.db.dependencies import get_db
 from api.handlers import (
     PetitionHandler,
     ConnectionManager
-    )
+)
 from api.env import settings
-from api.security import get_current_clerk
+from api.security import get_clerk_connection_manager
 
-manager = ConnectionManager()
+ClerkConnectionManager = get_clerk_connection_manager()
 
 router = APIRouter()
+
+logger = logging.getLogger(__name__)
 
 class UUIDEncoder(JSONEncoder):
     def default(self, obj):
@@ -30,11 +34,13 @@ class UUIDEncoder(JSONEncoder):
         if isinstance(obj, date):
             return obj.isoformat()  # Convert date to ISO 8601 string
         return super().default(obj)
-    
+
+
 def get_petition_handler(
         db: Session = Depends(get_db)
-        ) -> PetitionHandler:
+) -> PetitionHandler:
     return PetitionHandler(db)
+
 
 def get_all_clerks():
     # when there are clerks this function will get clerk id's from hr-login-backend
@@ -44,51 +50,71 @@ def get_all_clerks():
         return response.json().get("clerks", [])
     return []
 
-async def send_serialized_data_to_clerks(data: List[Dict]):
+
+async def send_serialized_data_to_clerks(data: List[Dict], manager: ConnectionManager = ClerkConnectionManager):
     """
     Send data to a specific WebSocket connection using the user ID.
-
     """
     clerks = get_all_clerks()
     for client_id in clerks:
         await manager.send_message(
             client_id,
-             dumps({
+            dumps({
                 "type": "updated_petitions",
                 "data": data
             }, cls=UUIDEncoder)
         )
 
+
 @router.websocket("/ws/{client_id}")
-async def websocket_endpoint(
-    client_id: str,
-    websocket: WebSocket, 
-    handler: PetitionHandler = Depends(get_petition_handler),
-    user: dict = Depends(get_current_clerk),
-    ):
+async def websocket_endpoint(client_id: str, websocket: WebSocket,
+                             manager: ConnectionManager = Depends(get_clerk_connection_manager),
+                             handler: PetitionHandler = Depends(get_petition_handler)) -> None:
+
+    """
+    Websocket for clerks to receive real-time updates on petitions. The clerk must authenticate using a token sent in
+    the first message after connecting. If authentication is successful, the clerk will receive the current list of
+    pending petitions and any subsequent updates. If authentication fails or if there is a timeout while waiting
+    for the authentication message, the connection will be closed with an appropriate status code.
+    """
+
     clerk_ids = get_all_clerks()
     if client_id not in clerk_ids:
         raise RuntimeError(f"Invalid user id: {client_id}")
 
     await manager.connect(client_id, websocket)
+    try:
+        data = await asyncio.wait_for(websocket.receive_json(), timeout=30.0)
+    except asyncio.TimeoutError:
+        logger.warning(f"Authentication timeout for client_id: {client_id}")
+        await manager.disconnect(client_id, 1008)
+        return
 
-    # On connection, send the current petitions to the client
-    petitions = handler.get_petitions_clerk()
-    await manager.send_message(
-        client_id, 
-        dumps({
+    if data.get("type", "") == "auth" and data.get("token", None) is not None:
+        manager.authenticate(data.get("token"))
+
+    if manager.is_authenticated:
+        petitions = handler.get_petitions_clerk()
+        await manager.send_message(
+            client_id,
+            dumps({
                 "type": "new_petition",
-                "data": [petition.dict(by_alias=True, exclude_none=True) for petition in petitions] 
+                "data": [petition.dict(by_alias=True, exclude_none=True) for petition in petitions]
             }, cls=UUIDEncoder)
-    )
+        )
+    else:
+        await manager.disconnect(client_id, 1008)
+        return
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_message(client_id, f"Echo: {data}")
+            await websocket.receive_json()
     except WebSocketDisconnect:
-        manager.disconnect(client_id)
+        await manager.disconnect(client_id, 1008)
+
 
 import json
+
+
 @router.post("/send-data/{user_id}")
 async def send_data(user_id: str):
     """
