17e02b2df2310966a7ea2fc77b8f8094515c2e5d
Christian Grossmüller <chgad.games@gmail.com>
2026-06-15 16:15:40 +0200

    Add authentication to ConnectionManager.

--- api/handlers/socker_handler.py
+++ api/handlers/socker_handler.py
@@ -1,22 +1,34 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
-from typing import Dict
+from collections.abc import Callable
 
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from typing import Dict, Any
 
 
 class ConnectionManager:
-    def __init__(self):
+    def __init__(self, authenticator_func: Callable[[str], Any]):
         self.active_connections: Dict[str, WebSocket] = {}
+        self.is_authenticated = False
+        self.authenticator = authenticator_func
+
+    def authenticate(self, token: str):
+        try:
+            self.authenticator(token)
+        except Exception as e:
+            raise e
+        else:
+            self.is_authenticated = True
 
     async def connect(self, client_id: str, websocket: WebSocket):
         await websocket.accept()
         self.active_connections[client_id] = websocket
 
-    def disconnect(self, client_id: str):
+    async def disconnect(self, client_id: str, code: int):
         if client_id in self.active_connections:
+            await self.active_connections[client_id].close(code)
             del self.active_connections[client_id]
 
     async def send_message(self, client_id: str, message: str):
-        if client_id in self.active_connections:
+        if client_id in self.active_connections and self.is_authenticated:
             websocket = self.active_connections[client_id]
             await websocket.send_text(message)
 
