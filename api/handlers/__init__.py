66e23c406412815a3c4075854cf53f2e53054f25
Christian Grossmüller <chgad.games@gmail.com>
2026-06-15 16:34:06 +0200

    Restructure location of ConnectionManager.

--- api/handlers/__init__.py
+++ api/handlers/__init__.py
@@ -2,7 +2,7 @@ from .petition_handler import PetitionHandler
 from .document_handler import StudentDocumentHandler
 from .employee_handler import EmployeeHandler
 from .email_handler import EmailHandler
-from .socker_handler import ConnectionManager
+from api.websockets.managers.socker_handler import ConnectionManager
 from api.security import auth_clerk_from_token
 
 ClerkConnectionManager = ConnectionManager(auth_clerk_from_token)
