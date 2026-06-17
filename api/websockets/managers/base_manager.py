e6edd1b8b3fa3ea2180fe607877894e1f9a6eabc
Christian Grossmüller <chgad.games@gmail.com>
2026-06-17 18:31:05 +0200

    Add is_authenticated method, broadcast to authenticated connections, make remove method robust.

--- api/websockets/managers/base_manager.py
+++ api/websockets/managers/base_manager.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass, field
 from fastapi import WebSocket
 from typing import Dict, Any
 
+from starlette.websockets import WebSocketState
+
 
 @dataclass
 class Connection:
@@ -20,7 +22,9 @@ class ConnectionArray:
         self.connections[connection.client_id] = connection
 
     async def remove(self, client_id: str, code: int = 1008):
-        await self.connections[client_id].websocket.close(code)
+        connection = self.connections.get(client_id)
+        if connection and connection.websocket.application_state == WebSocketState.CONNECTED:
+            await self.connections[client_id].websocket.close(code)
         del self.connections[client_id]
 
     def authenticate_connection(self, client_id: str):
@@ -39,13 +43,17 @@ class WebsocketConnectionManager:
         self.active_connections: ConnectionArray = ConnectionArray()
         self.authenticator = authenticator_func
 
-    def authenticate(self, token: str, client_id: str):
+    def authenticate(self, client_id: str, token: str,):
         try:
             self.authenticator(token)
         except Exception as e:
             raise e
         else:
             self.active_connections.authenticate_connection(client_id)
+
+    def is_authenticated(self, client_id: str) -> bool:
+        connection = self.active_connections.get_connection(client_id)
+        return connection.is_authenticated if connection else False
 
     async def connect(self, client_id: str, websocket: WebSocket):
         await websocket.accept()
@@ -64,4 +72,5 @@ class WebsocketConnectionManager:
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            await connection.websocket.send_text(message)
+            if connection.is_authenticated:
+                await connection.websocket.send_text(message)
