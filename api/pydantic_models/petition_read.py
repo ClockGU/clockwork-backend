4c9e32fedb4262f7c92e5186b82efbff92ffa922
Christian Grossmüller <chgad.games@gmail.com>
2026-06-30 17:00:48 +0200

    Make PetitionBaseRead use PetitionBase.

--- api/pydantic_models/petition_read.py
+++ api/pydantic_models/petition_read.py
@@ -7,27 +7,12 @@ import re
 
 from api.consts import PetitionStatus
 from .budget_position import BudgetPositionRead
+from .petition import PetitionBase
 
 
 # Base class for shared fields
-class PetitionBaseRead(SQLModel):
+class PetitionBaseRead(PetitionBase):
     id: uuid.UUID
-    start_date: date
-    end_date: date
-    minutes: int
-    student_username: str
-    supervisor_mail: Optional[str] = None
-    time_exce_name: Optional[str] = None
-    time_exce_start: Optional[date] = None
-    time_exce_end: Optional[date] = None
-    time_exce_time: Optional[int] = None
-    duration_exce_name: Optional[str] = None
-    duration_exce_start: Optional[date] = None
-    duration_exce_end: Optional[date] = None
-    time_exce_course: Optional[bool] = None  
-    duration_exce_course: Optional[bool] = None 
-    status: PetitionStatus = PetitionStatus.SUPERVISOR_ACTION
-
 
 # PetitionReadBase for clerks/supervisors
 class PetitionRead(PetitionBaseRead):
