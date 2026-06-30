eb5469316f121235f99e880573ea23b0995c0ef0
Christian Grossmüller <chgad.games@gmail.com>
2026-06-30 15:26:09 +0200

    Extract BudgetPosition validation logic to ValidationMixin.

--- api/pydantic_models/budget_position.py
+++ api/pydantic_models/budget_position.py
@@ -1,8 +1,7 @@
 from typing import Optional
 import uuid
-from pydantic import BaseModel, field_validator, model_validator
-import re
-
+from pydantic import BaseModel
+from validators.budget_position_validators import BudgetPositionValidationMixin
 from sqlmodel import SQLModel
 
 
@@ -11,23 +10,8 @@ class BudgetPositionBase(SQLModel):
     budget_approver: str
     percentage: float
 
-class BudgetPositionCreate(BudgetPositionBase):
-    budget_position: str
-    budget_approver: str
-    percentage: float  
-
-    @field_validator("budget_approver")
-    def validate_budget_approver(cls, budget_approver):
-        # Ensure budget_approver is a valid email format
-        if not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", budget_approver):
-            raise ValueError("budget_approver must be a valid email address")
-        return budget_approver
-
-    @field_validator("percentage")
-    def validate_percentage(cls, percentage):
-        if percentage <= 0 or percentage > 100:
-            raise ValueError("percentage must be between 0 and 100")
-        return percentage
+class BudgetPositionCreate(BudgetPositionBase,BudgetPositionValidationMixin):
+    pass
 
 class BudgetPositionRead(BaseModel):
     id: uuid.UUID
---
+++ api/pydantic_models/validators/budget_position_validators.py
@@ -0,0 +1,19 @@
+import re
+
+from pydantic import field_validator
+
+
+class BudgetPositionValidationMixin:
+    @field_validator("budget_approver")
+    def validate_budget_approver(cls, budget_approver):
+        # Ensure budget_approver is a valid email format
+        if not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", budget_approver):
+            raise ValueError("budget_approver must be a valid email address")
+        return budget_approver
+
+
+    @field_validator("percentage")
+    def validate_percentage(cls, percentage):
+        if percentage <= 0 or percentage > 100:
+            raise ValueError("percentage must be between 0 and 100")
+        return percentage
