306da0bf2bd407a9f5c850578d661b4d8a5615ab
Christian Grossmüller <chgad.games@gmail.com>
2026-06-30 15:04:14 +0200

    Remove unused PetitionRead definition.

--- api/pydantic_models/petition.py
+++ api/pydantic_models/petition.py
@@ -34,32 +34,6 @@ class PetitionBase(SQLModel):
     duration_exce_end: Optional[date] = None
 
 
-class PetitionRead(SQLModel):
-    id: uuid.UUID
-    user_account: Optional[uuid.UUID] = None
-    org_unit: str
-    eos_number: str
-    start_date: date
-    end_date: date
-    minutes: int
-    ba_degree: bool
-    budget_position: str
-    budget_approver: str
-    student_username: str
-
-    status: PetitionStatus = PetitionStatus.SUPERVISOR_ACTION
-
-    time_exce_student: Optional[bool] = None
-    time_exce_course: Optional[bool] = None
-    time_exce_name: Optional[str] = None
-    time_exce_start: Optional[date] = None
-    time_exce_end: Optional[date] = None
-
-    duration_exce_course: Optional[bool] = None
-    duration_exce_name: Optional[str] = None
-    duration_exce_start: Optional[date] = None
-    duration_exce_end: Optional[date] = None
-
 class PetitionStudentBase(SQLModel):
     start_date: date
     end_date: date
