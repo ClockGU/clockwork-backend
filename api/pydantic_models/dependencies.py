8762945e7a7a929957b8e93c0d82f0bacbc484ad
Christian Grossmüller <chgad.games@gmail.com>
2026-07-06 16:39:08 +0200

    Use dependency to fetch specified petition to reduce code repetition.

--- api/pydantic_models/dependencies.py
+++ api/pydantic_models/dependencies.py
@@ -1,27 +1,20 @@
-from uuid import UUID
-
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from sqlmodel import Session
 
 from api.db.dependencies import get_db
-from api.db.managers import PetitionManager
+from api.db.managers.dependencies import get_specified_petition
+from api.db.schema import Petition
 from api.pydantic_models import PetitionSupervisorUpdate, PetitionClerkUpdate
 
 
-def get_supervisor_petition_update_model(petition_id:UUID, body:dict, db: Session = Depends(get_db)) -> PetitionSupervisorUpdate:
+def get_supervisor_petition_update_model(petition: Petition = Depends(get_specified_petition), body:dict, db: Session = Depends(get_db)) -> PetitionSupervisorUpdate:
     """
     Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
     """
-    existing_petition = PetitionManager(db).get_petition(petition_id)
-    if not existing_petition:
-        raise HTTPException(status_code=404, detail=f"Petition with ID{str(petition_id)} not found")
-    return PetitionSupervisorUpdate.model_validate(body, existing_petition)
+    return PetitionSupervisorUpdate.model_validate(body, petition)
 
-def get_clerk_petition_update_model(petition_id:UUID, body:dict, db: Session = Depends(get_db)) -> PetitionClerkUpdate:
+def get_clerk_petition_update_model(petition: Petition = Depends(get_specified_petition), body:dict, db: Session = Depends(get_db)) -> PetitionClerkUpdate:
     """
     Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
     """
-    existing_petition = PetitionManager(db).get_petition(petition_id)
-    if not existing_petition:
-        raise HTTPException(status_code=404, detail=f"Petition with ID{str(petition_id)} not found")
-    return PetitionClerkUpdate.model_validate(body, existing_petition)
+    return PetitionClerkUpdate.model_validate(body, petition)
