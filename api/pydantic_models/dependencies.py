8ecb14df70dbb9199c613e9c5e33ab8f46ea0ecf
Christian Grossmüller <chgad.games@gmail.com>
2026-07-06 18:07:33 +0200

    Fix parameter ordering.

--- api/pydantic_models/dependencies.py
+++ api/pydantic_models/dependencies.py
@@ -7,13 +7,13 @@ from api.db.schema import Petition
 from api.pydantic_models import PetitionSupervisorUpdate, PetitionClerkUpdate
 
 
-def get_supervisor_petition_update_model(petition: Petition = Depends(get_specified_petition), body:dict, db: Session = Depends(get_db)) -> PetitionSupervisorUpdate:
+def get_supervisor_petition_update_model(body:dict, petition: Petition = Depends(get_specified_petition), db: Session = Depends(get_db)) -> PetitionSupervisorUpdate:
     """
     Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
     """
     return PetitionSupervisorUpdate.model_validate(body, petition)
 
-def get_clerk_petition_update_model(petition: Petition = Depends(get_specified_petition), body:dict, db: Session = Depends(get_db)) -> PetitionClerkUpdate:
+def get_clerk_petition_update_model(body:dict, petition: Petition = Depends(get_specified_petition), db: Session = Depends(get_db)) -> PetitionClerkUpdate:
     """
     Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
     """
