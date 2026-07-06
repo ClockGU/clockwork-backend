2f882e7f2b241c056e07e7ea3ca437f380ea5b04
Christian Grossmüller <chgad.games@gmail.com>
2026-07-06 16:35:47 +0200

    Add Dependecy function to provide PetitionClerkUpdate.

--- api/pydantic_models/dependencies.py
+++ api/pydantic_models/dependencies.py
@@ -5,7 +5,7 @@ from sqlmodel import Session
 
 from api.db.dependencies import get_db
 from api.db.managers import PetitionManager
-from api.pydantic_models import PetitionSupervisorUpdate
+from api.pydantic_models import PetitionSupervisorUpdate, PetitionClerkUpdate
 
 
 def get_supervisor_petition_update_model(petition_id:UUID, body:dict, db: Session = Depends(get_db)) -> PetitionSupervisorUpdate:
@@ -16,3 +16,12 @@ def get_supervisor_petition_update_model(petition_id:UUID, body:dict, db: Sessio
     if not existing_petition:
         raise HTTPException(status_code=404, detail=f"Petition with ID{str(petition_id)} not found")
     return PetitionSupervisorUpdate.model_validate(body, existing_petition)
+
+def get_clerk_petition_update_model(petition_id:UUID, body:dict, db: Session = Depends(get_db)) -> PetitionClerkUpdate:
+    """
+    Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
+    """
+    existing_petition = PetitionManager(db).get_petition(petition_id)
+    if not existing_petition:
+        raise HTTPException(status_code=404, detail=f"Petition with ID{str(petition_id)} not found")
+    return PetitionClerkUpdate.model_validate(body, existing_petition)
