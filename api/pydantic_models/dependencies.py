e60d42e8ca685666469f8037449927fc76b187cd
Christian Grossmüller <chgad.games@gmail.com>
2026-07-06 18:37:44 +0200

    Implement dependency to provide PetitionStudenUpdate model.

--- api/pydantic_models/dependencies.py
+++ api/pydantic_models/dependencies.py
@@ -4,7 +4,7 @@ from sqlmodel import Session
 from api.db.dependencies import get_db
 from api.db.managers.dependencies import get_specified_petition
 from api.db.schema import Petition
-from api.pydantic_models import PetitionSupervisorUpdate, PetitionClerkUpdate
+from api.pydantic_models import PetitionSupervisorUpdate, PetitionClerkUpdate, PetitionStudentUpdate
 
 
 def get_supervisor_petition_update_model(body:dict, petition: Petition = Depends(get_specified_petition), db: Session = Depends(get_db)) -> PetitionSupervisorUpdate:
@@ -18,3 +18,9 @@ def get_clerk_petition_update_model(body:dict, petition: Petition = Depends(get_
     Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
     """
     return PetitionClerkUpdate.model_validate(body, petition)
+
+def get_student_petition_update_model(body:dict, petition: Petition = Depends(get_specified_petition), db: Session = Depends(get_db)) -> PetitionStudentUpdate:
+    """
+    Returns a PetitionSupervisorUpdate model instance with the provided petition_id and body.
+    """
+    return PetitionStudentUpdate.model_validate(body, petition)
