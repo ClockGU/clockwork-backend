e499f0c49c4566647aef9db0391f3182b2e86926
Christian Grossmüller <chgad.games@gmail.com>
2026-07-10 17:46:32 +0200

    Create BudgetPositionHandler for budget position related logic.

---
+++ api/handlers/budget_position_handler.py
@@ -0,0 +1,75 @@
+from typing import Optional, Self
+
+from sqlmodel import Session
+
+from api.db.managers.budget_position_manager import BudgetPositionManager
+from api.db.schema import BudgetPosition, Petition
+from api.handlers import EmailHandler
+from api.handlers.exception_handler import ExceptionHandler
+from tests.unit.conftests import budget_position
+
+
+class BudgetPositionsHandler:
+
+    def __init__(
+        self,
+        db: Session,
+        object_instances: Optional[list[BudgetPosition]] = None,
+        petition: Optional[Petition] = None,
+    ):
+        self.manager = BudgetPositionManager(db)
+        self.db = db
+        self.exc = ExceptionHandler()
+        self._object_instances = object_instances
+        self._petition = petition
+
+    def get_objects(self):
+        if not self._object_instances:
+            raise RuntimeError(
+                "Calling get_object() is not allowed when no existing objects was provided at initialization."
+            )
+        return self._object_instances
+
+    def get_petition(self):
+        if not self._petition:
+            raise RuntimeError(
+                "Calling get_petition() is not allowed when no existing petition was provided at initialization."
+            )
+        return self._petition
+
+    @classmethod
+    def from_existing_object(
+        cls, db: Session, object_instances: list[BudgetPosition]
+    ) -> Self:
+        """
+        Explicitly create a PetitionHandler instance from an existing Petition object.
+        """
+        return cls(db, object_instances)
+
+    @classmethod
+    def from_petition(cls, db: Session, petition: Petition) -> Self:
+        """
+        Explicitly create a PetitionHandler instance from an existing Petition object.
+        """
+        self = cls(db, petition=petition)
+        budget_positions = self.manager.get_budget_positions_by_petition(petition)
+        self.__set_object_instances(budget_positions)
+        return self
+
+    def __set_object_instances(self, value: list[BudgetPosition]):
+        self._object_instances = value
+
+    def reset_approval_status(self):
+        """
+        Reset the approval status of all budget positions associated with the petition to False.
+        Send email notifications to budget positions about the status update.
+        """
+        budget_positions = self.get_objects()
+        for budget_position in budget_positions:
+            self.manager.update_budget_position_status(budget_position, False)
+        try:
+            email_handler = EmailHandler(self.get_petition())
+            email_handler.send_budget_position_update_emails(budget_positions)
+        except Exception as e:
+            raise self.exc.internal_error("sending budget position update emails", e)
+
