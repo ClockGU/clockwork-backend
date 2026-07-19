a3acf359dbacb21f500875611a462f22848fd141
Christian Grossmüller <chgad.games@gmail.com>
2026-07-19 15:39:20 +0200

    Implement dependency to provide an employee handler.

--- api/handlers/dependencies.py
+++ api/handlers/dependencies.py
@@ -4,10 +4,9 @@ from fastapi import Depends
 from sqlmodel import Session
 
 from api.db.dependencies import get_db
-from api.db.managers.dependencies import get_specified_petition
-from api.db.schema import Petition
-from api.handlers import PetitionHandler
-
+from api.db.managers.dependencies import get_specified_petition, get_specified_employee
+from api.db.schema import Petition, Employee
+from api.handlers import PetitionHandler, EmployeeHandler
 
 def get_petition_handler(
     petition: Optional[Petition] = Depends(get_specified_petition),
@@ -20,3 +19,15 @@ def get_petition_handler(
     if petition:
         return PetitionHandler.from_existing_object(db, petition)
     return PetitionHandler(db)
+
+def get_employee_handler(
+        employee: Optional[Employee] = Depends(get_specified_employee),
+        db: Session = Depends(get_db)
+)-> EmployeeHandler:
+    """
+    Dependency function to provide a EmployeeHandler depending on whether a emplyoee was
+    specified in the request.
+    """
+    if employee:
+        return EmployeeHandler.from_existing_object(db, employee)
+    return EmployeeHandler(db)
