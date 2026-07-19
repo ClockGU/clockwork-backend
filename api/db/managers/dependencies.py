7a1b3a4b099f2d050171e2e84db3deedfa23f079
Christian Grossmüller <chgad.games@gmail.com>
2026-07-19 15:37:18 +0200

    implement dependency to retrieve specified employee.

--- api/db/managers/dependencies.py
+++ api/db/managers/dependencies.py
@@ -7,8 +7,8 @@ from starlette.requests import Request
 
 from api.db.dependencies import get_db
 from api.db.managers import PetitionManager
-from api.db.schema import Petition
-
+from api.db.managers.emploeyee_manager import EmployeeManager
+from api.db.schema import Petition, Employee
 
 def get_specified_petition(
     petition_id: Optional[UUID], request: Request,db: Session = Depends(get_db)
@@ -26,3 +26,20 @@ def get_specified_petition(
             status_code=404, detail=f"Petition with ID{str(petition_id)} not found"
         )
     return existing_petition
+
+def get_specified_employee(
+    employee_id: Optional[UUID], request: Request,db: Session = Depends(get_db)
+) -> Optional[Employee]:
+    """
+    Dependency function to retrieve a specified employee if a employee_id was provided in the path.
+    :raises: HTTPException with status code 404 if the employee is not found.
+    """
+    if not employee_id:
+        return None
+
+    existing_employee = EmployeeManager(db).get_employee(employee_id)
+    if not existing_employee:
+        raise HTTPException(
+            status_code=404, detail=f"Petition with ID{str(employee_id)} not found"
+        )
+    return existing_employee
