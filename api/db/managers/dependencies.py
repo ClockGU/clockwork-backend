306c787fce0fe39867d62c46b0efb7f6d652cb2d
Christian Grossmüller <chgad.games@gmail.com>
2026-08-13 16:43:41 +0200

    Streamline get_emlpoyee_handler dependecy.

--- api/db/managers/dependencies.py
+++ api/db/managers/dependencies.py
@@ -29,23 +29,19 @@ def get_specified_petition(
         )
     return existing_petition
 
-
-def get_employee_for_student(
-    student: dict = Depends(get_current_student), db: Session = Depends(get_db)
+def get_specified_employee(
+    employee_id: Optional[UUID] = None, db: Session = Depends(get_db)
 ) -> Optional[Employee]:
     """
     Dependency function to retrieve a specified employee if a employee_id was provided in the path.
     :raises: HTTPException with status code 404 if the employee is not found.
     """
-    if not student:
+    if not employee_id:
         return None
 
-    existing_employee = EmployeeManager(db).get_employee_by_user_account(
-        student.get("sub")
-    )
+    existing_employee = EmployeeManager(db).get_employee(employee_id)
     if not existing_employee:
         raise HTTPException(
-            status_code=404,
-            detail=f"Petition with ID{str(student.get("sub"))} not found",
+            status_code=404, detail=f"Employee with ID{str(employee_id)} not found"
         )
     return existing_employee
