339126346da239128476da4a98a1ba287cf9c48a
Christian Grossmüller <chgad.games@gmail.com>
2026-08-14 12:50:19 +0200

    Remove worktime exception field from pydantic model for petition.

--- api/pydantic_models/petition.py
+++ api/pydantic_models/petition.py
@@ -12,12 +12,10 @@ class PetitionExceptionBase(SQLModel):
     time_exce_student: Optional[bool] = None
     time_exce_course: Optional[bool] = None
     time_exce_name: Optional[str] = None
-    time_exce_start: Optional[date] = None
-    time_exce_end: Optional[date] = None
     time_exce_time: Optional[int] = None
 
     duration_exce_course: Optional[bool] = None
-    duration_exec_student: Optional[bool] = None
+    duration_exce_student: Optional[bool] = None
     duration_exce_name: Optional[str] = None
     duration_exce_start: Optional[date] = None
     duration_exce_end: Optional[date] = None
