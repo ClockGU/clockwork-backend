7f847b6c5d406c0edd24d583e7956f653ecaf033
Christian Grossmüller <chgad.games@gmail.com>
2026-08-31 11:09:37 +0200

    Add response model.

--- api/routers/prev_employment.py
+++ api/routers/prev_employment.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Body
 from sqlmodel import Session
 
 from api.db.dependencies import get_db
+from api.db.schema.prev_employment import PrevEmployment
 from api.handlers.prev_employment_handler import PrevEmploymentHandler
 from api.security import get_current_student
 
@@ -9,7 +10,7 @@ from api.security import get_current_student
 
 router = APIRouter()
 
-@router.post("/prev_employments")
+@router.post("/prev_employments",response_model=PrevEmployment)
 def create_new_employment(
     body: dict = Body(...),
     session: Session = Depends(get_db),
