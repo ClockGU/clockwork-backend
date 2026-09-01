47aed76ea137090d273280e3d426936fab0cb550
Christian Grossmüller <chgad.games@gmail.com>
2026-09-01 14:48:09 +0200

    Add Update pydantic model.

--- api/pydantic_models/prev_employment.py
+++ api/pydantic_models/prev_employment.py
@@ -2,7 +2,8 @@ import uuid
 from datetime import date
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
 
 class PrevEmploymentCreate(BaseModel):
     user_account: uuid.UUID
@@ -17,3 +18,10 @@ class ReadPrevEmploymentCreate(BaseModel):
     end: date
     employer_name: Optional[str] = None
     proof: Optional[str] = None
+
+class PrevEmploymentUpdate(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    start: Optional[date] = None
+    end: Optional[date] = None
+    employer_name: Optional[str] = None
